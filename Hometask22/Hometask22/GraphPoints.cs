7ed83aa9ad6928e4459833ce542ca2c2d7f214c4
Oksana Levkovskaya <oksizhura@gmail.com>
2021-12-19 16:03:36 +0300

    notes

---
+++ Hometask22/Hometask22/GraphPoints.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hometask22
+{
+    public class GraphPoints
+    {
+        private string pointName;
+        private int coordinateX;
+        private int coordinateY;
+    
+
+    public GraphPoints (string pn, int cx, int cy)
+        {
+        pointName = pn;
+        coordinateX = cx;
+        coordinateY = cy;   
+
+        }
+        
+        public string GetPoint()
+        {
+            return "Point Name is: " + pointName + ","
+                    + " Coordinate X - " + coordinateX
+                    + " Coordinate Y - " + coordinateY;
+        }
+    }
+}
---
+++ Hometask22/Hometask22/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hometask22
+{
+    public class Point
+    {
+       
+
+        static void Main(string[] args)
+
+        {
+            GraphPoints point1 = new GraphPoints ("P1", 1, 1);
+            GraphPoints point2 = new GraphPoints ("P2", 2, 2);
+            GraphPoints point3 = new GraphPoints ("P3",3,3);
+            GraphPoints point4 = new GraphPoints("P1", 1, 1);
+            GraphPoints point5 = new GraphPoints("P2", 2, 2);
+            GraphPoints point6 = new GraphPoints("P3", 3, 3);
+            GraphPoints point7 = new GraphPoints("P1", 1, 1);
+            GraphPoints point8 = new GraphPoints("P2", 2, 2);
+            GraphPoints point9 = new GraphPoints("P3", 3, 3);
+            GraphPoints point10 = new GraphPoints("P1", 1, 1);
+
+            GraphPoints [] graphPoints = { point1, point2, point3, point4, point5, point6,
+                                           point7, point8, point9, point10};
+
+            foreach (GraphPoints p in graphPoints)
+            {
+                Console.WriteLine (p.GetPoint());
+            }
+
+         
+
+            
+        }
+    }
+}
